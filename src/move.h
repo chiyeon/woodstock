7a983375429062326013f1363502cde395bce48c
Benjamin Wong <me@benjiwong.com>
2023-07-14 18:13:05 -0700

    Began basis for move flags

--- src/move.h
+++ src/move.h
@@ -3,12 +3,21 @@
 
 #include "constants.h"
 
+enum MoveFlag
+{
+    PAWN_DOUBLE,
+    EN_PASSANT,
+    PROMOTION,
+    CASTLE,
+};
+
 struct Move
 {
     int from;
     int to;
     Piece piece;
     Piece captured;
+    MoveFlag * flags;
 
     Move(int from, int to, Piece piece)
         : from(from)
